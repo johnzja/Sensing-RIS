20c503ee55b059d71924eb1bacc969250923fef9
lkz <lkz18>
2021-12-07 16:35:39 +0800

    update CE

--- code/CE_sparsity.m
+++ code/CE_sparsity.m
@@ -10,11 +10,26 @@ UM1=(1/sqrt(M1))*exp(-1i*2*pi*(0:M1-1)'*(-(M1-1)/2:1:(M1/2))*(2/M1)/2);
 UM2=(1/sqrt(M2))*exp(-1i*2*pi*(0:M2-1)'*(-(M2-1)/2:1:(M2/2))*(2/M2)/2);
 UM=kron(UM1,UM2);
 
-H = zeros(N, M, K);
-for k = 1:K
-    fk = f(:, k);
-    Hk = G'*diag(fk);
-    H(:,:,k)=(UM'*Hk*(UN.')')';
-    imagesc(abs(H(:,:,k)));
+sample = 20;
+Q_all = 32:16:128;
+for s = 1:sample
+    fprintf("s = %2d\n", s);
+    H = zeros(N, M, K);
+    for k = 1:K
+        fk = f(:, k);
+        Hk = G'*diag(fk);
+        H(:,:,k)=(UM'*Hk*(UN.')')';
+        %imagesc(abs(H(:,:,k)));
+    end
+    for i = 1:length(Q_all)
+        Q = Q_all(i);
+        Y = zeros(Q, M, K);
+        W = ((rand(N,Q)>0.5)*2-1)/sqrt(N);
+        A = (UN*W)';
+        for k = 1:K
+            noise = sqrt(sigma2)*(randn(Q,M)+1i*randn(Q,M))/sqrt(2);
+            Y(:,:,k) = A*H(:,:,k)+noise;
+        end
+    end
 end
 end
